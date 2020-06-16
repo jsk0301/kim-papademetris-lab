114d64d0539b96abb00bd3fe4f9d07a0ae18f24f
John Kim <john.sj.kim@yale.edu>
2020-06-16 17:30:33 -0400

    Randcropbyposnegid, NormalizeIntensity

---
+++ lv_masking/models/6-16-2020_normalize/MONAIUNet.py
@@ -0,0 +1,173 @@
+# MONAI UNet imports
+import torch
+import torch.nn as nn
+from pytorch_lightning.core.lightning import LightningModule
+
+from monai.networks.blocks.convolutions import Convolution, ResidualUnit
+from monai.networks.layers.factories import Norm, Act
+from monai.networks.layers.simplelayers import SkipConnection
+from monai.utils import export
+from monai.utils.aliases import alias
+
+
+### UNet Model
+# Most of the code is copied from MONAI's implenetation of a UNet
+class UNet(LightningModule):
+    def __init__(
+        self,
+        dimensions,
+        in_channels,
+        out_channels,
+        channels,
+        strides,
+        criterion,
+        hparams,
+        kernel_size=3,
+        up_kernel_size=3,
+        num_res_units=0,
+        act=Act.PRELU,
+        norm=Norm.INSTANCE,
+        dropout=0,
+    ):
+        super().__init__()
+        assert len(channels) == (len(strides) + 1)
+        self.dimensions = dimensions
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.channels = channels
+        self.strides = strides
+        self.criterion = criterion
+        self.hparams = hparams
+        self.kernel_size = kernel_size
+        self.up_kernel_size = up_kernel_size
+        self.num_res_units = num_res_units
+        self.act = act
+        self.norm = norm
+        self.dropout = dropout
+
+        def _create_block(inc, outc, channels, strides, is_top):
+            """
+            Builds the UNet structure from the bottom up by recursing down to the bottom block, then creating sequential
+            blocks containing the downsample path, a skip connection around the previous block, and the upsample path.
+            """
+            c = channels[0]
+            s = strides[0]
+
+            if len(channels) > 2:
+                subblock = _create_block(
+                    c, c, channels[1:], strides[1:], False
+                )  # continue recursion down
+                upc = c * 2
+            else:
+                # the next layer is the bottom so stop recursion, create the bottom layer as the sublock for this layer
+                subblock = self._get_bottom_layer(c, channels[1])
+                upc = c + channels[1]
+
+            down = self._get_down_layer(
+                inc, c, s, is_top
+            )  # create layer in downsampling path
+            up = self._get_up_layer(
+                upc, outc, s, is_top
+            )  # create layer in upsampling path
+
+            return nn.Sequential(down, SkipConnection(subblock), up)
+
+        self.model = _create_block(
+            in_channels, out_channels, self.channels, self.strides, True
+        )
+
+    def _get_down_layer(self, in_channels, out_channels, strides, is_top):
+        if self.num_res_units > 0:
+            return ResidualUnit(
+                self.dimensions,
+                in_channels,
+                out_channels,
+                strides,
+                self.kernel_size,
+                self.num_res_units,
+                self.act,
+                self.norm,
+                self.dropout,
+            )
+        else:
+            return Convolution(
+                self.dimensions,
+                in_channels,
+                out_channels,
+                strides,
+                self.kernel_size,
+                self.act,
+                self.norm,
+                self.dropout,
+            )
+
+    def _get_bottom_layer(self, in_channels, out_channels):
+        return self._get_down_layer(in_channels, out_channels, 1, False)
+
+    def _get_up_layer(self, in_channels, out_channels, strides, is_top):
+        conv = Convolution(
+            self.dimensions,
+            in_channels,
+            out_channels,
+            strides,
+            self.up_kernel_size,
+            self.act,
+            self.norm,
+            self.dropout,
+            conv_only=is_top and self.num_res_units == 0,
+            is_transposed=True,
+        )
+
+        if self.num_res_units > 0:
+            ru = ResidualUnit(
+                self.dimensions,
+                out_channels,
+                out_channels,
+                1,
+                self.kernel_size,
+                1,
+                self.act,
+                self.norm,
+                self.dropout,
+                last_conv_only=is_top,
+            )
+            return nn.Sequential(conv, ru)
+        else:
+            return conv
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+    # Lightning training
+    def training_step(self, batch, batch_idx):
+        inputs, labels = (
+            batch["image"],
+            batch["mask"],
+        )
+        inputs = inputs.squeeze(4)
+        labels = labels.squeeze(4)
+        outputs = self(inputs)
+        loss = self.criterion(outputs, labels)
+        tensorboard_logs = {"loss/train": loss}
+        return {"loss": loss, "log": tensorboard_logs}
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
+
+    # Lightning validation
+    def validation_step(self, batch, batch_idx):
+        inputs, labels = (
+            batch["image"],
+            batch["mask"],
+        )
+        inputs = inputs.squeeze(4)
+        labels = labels.squeeze(4)
+        outputs = self(inputs)
+        loss = self.criterion(outputs, labels)
+        return {"val_loss": loss}
+
+    def validation_epoch_end(self, outputs):
+        avg_loss = torch.stack([x["val_loss"] for x in outputs]).mean()
+        tensorboard_logs = {"loss/val": avg_loss}
+        return {"val_loss": avg_loss, "log": tensorboard_logs}
---
+++ lv_masking/models/6-16-2020_normalize/train.sh
@@ -0,0 +1,2 @@
+python train_monai_unet2d.py \
+	--name 6-15-2020_celoss \
